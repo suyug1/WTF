0a7fc3e6fa93dd8baa88b73b154deb58b088291f
pgmrDohan <dohan.k764@gmail.com>
2024-05-29 20:16:15 +0900

    완료

--- nialbba.py
+++ nialbba.py
@@ -1,8 +1,14 @@
 def cal_score(score):
-
-
-
-
+	if score >= 90:
+		return "A"
+	elif score >= 80:
+		return "B"
+	elif score >= 70:
+		return "C"
+	elif score >= 60:
+		return "D"
+	else:
+		return "F"
 
 def main():
 
